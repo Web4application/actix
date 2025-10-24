19d2ea87659165be1986d92cfca7c64988a89940
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-24 19:14:24 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,6 @@
+sudo ln -s /etc/nginx/sites-available/actix.com /etc/nginx/sites-enabled/
+sudo nginx -t       # Test configuration
+sudo systemctl reload nginx
 
 git clone https://github.com/<web4application>/actix.git
 cd actix

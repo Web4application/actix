a0159fe1b60d6f6a54959ea51b7cd2307fe9300e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-24 14:37:08 +0100

    Update and rename index.html to index.rs

--- index.rs
+++ index.rs
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
+
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ site.title }}</title>
+  <title WEB4</title>
   <link rel="stylesheet" href="{{ '/assets/css/qubuhub.css' | relative_url }}">
   <style>
     /* Neon glow for headings */

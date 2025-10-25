50fd5a5a9ac42fca8f3d1461865514abe4ca84cf
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-25 05:57:00 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -10,10 +10,14 @@ docker-compose up --build -d
 docker-compose logs -f api
 
 sudo apt install certbot python3-certbot-nginx
-sudo certbot --nginx -d actix.com -d www.actix.com -d actix.io -d www.actix.io -d actix.rs -d www.actix.rs
+sudo certbot --nginx -d actix.com -d www.actix.com -d actix.io -d www.actix.io -d actix.ai -d www.actix.ai
 
 sudo apt install certbot python3-certbot-nginx
-sudo certbot --nginx -d actix.com -d www.actix.com
+sudo certbot --nginx -d actix.com -d www.actix.com -d actix.dev -d www.actix.dev -d reallms.dev -d www.reallms.dev 
 docker compose build
 docker compose up -d
 
+
+netstat -ano | findstr :8081 taskkill /PID typeyourPIDhere /F
+
+Read more here: https://locall.host/5557/

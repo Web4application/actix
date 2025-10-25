b7aec9b3500b0db17a67a765df7de90e537ed3ae
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-25 04:12:27 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,3 +11,9 @@ docker-compose logs -f api
 
 sudo apt install certbot python3-certbot-nginx
 sudo certbot --nginx -d actix.com -d www.actix.com -d actix.io -d www.actix.io -d actix.rs -d www.actix.rs
+
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d actix.com -d www.actix.com
+docker compose build
+docker compose up -d
+

1c1e3e3ba265cfe422ee79aaed0cf95da3a9084d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-23 16:18:45 +0100

    Update bash init.sh

--- bash init.sh
+++ bash init.sh
@@ -1,34 +1,33 @@
-pnpm approve-builds
-sudo ln -s /etc/nginx/sites-available/actix.com /etc/nginx/sites-enabled/
-sudo nginx -t       # Test configuration
-sudo systemctl reload nginx
+$pnpm approve-builds
+$sudo ln -s /etc/nginx/sites-available/actix.com /etc/nginx/sites-enabled/
+$sudo nginx -t       # Test configuration
+$sudo systemctl reload nginx
 
-git clone https://github.com/<web4application>/actix.git
+$ git clone https://github.com/<web4application>/actix.git
 cd actix
 
-docker-compose up --build -d
+$ docker-compose up --build -d
 
-docker-compose logs -f api
+$ docker-compose logs -f api
 
-sudo apt install certbot python3-certbot-nginx
-sudo certbot --nginx -d actix.com -d www.actix.com -d actix.io -d www.actix.io -d actix.ai -d www.actix.ai
+$ sudo apt install certbot python3-certbot-nginx
+$ sudo certbot --nginx -d actix.com -d www.actix.com -d actix.io -d www.actix.io -d actix.ai -d www.actix.ai
 
-sudo apt install certbot python3-certbot-nginx
-sudo certbot --nginx -d actix.com -d www.actix.com -d actix.dev -d www.actix.dev -d reallms.dev -d www.reallms.dev 
-docker compose build
-docker compose up -d
+$ sudo apt install certbot python3-certbot-nginx
+$sudo certbot --nginx -d actix.com -d www.actix.com -d actix.dev -d www.actix.dev -d reallms.dev -d www.reallms.dev 
+$docker compose build
+$docker compose up -d
 
 
-netstat -ano | findstr :8081 taskkill /PID typeyourPIDhere /F
+$netstat -nano | findstr :8000 taskkill /PID typeyourPIDhere /F
 
-Read more here: https://locall.host/5557/
-
-cd backend
+Read more here: https://127.0.0.1/5557/
+$cd backend
 ./mvnw spring-boot:run
 or
 
-mvn spring-boot:run
+$mvn spring-boot:run
 
 cd frontend
-npm install
-npm start
+$npm install
+$npm start

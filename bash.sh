405bbdb589d6402cb31fc57ff78d6f30c3adc2fc
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-23 19:57:05 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,6 @@
 npm run gendocs
+docs/build.rst
+mkdir -p docs
+rustdoc2rst target/doc/dev_ml.json > docs/dev.rst
+cargo doc --no-deps --document-private-items --output-format json
+cargo install rustdoc2rst
